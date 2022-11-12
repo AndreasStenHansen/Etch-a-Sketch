28ad38d799a171473e2de13c20a225ca023ff2fc
Andreas Sten Hansen <andreas_sten_hansen@hotmail.com>
2022-11-12 13:31:16 +0100

    Added function for creating multiple grid divs

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function ABC(){
+function createSingleGridDiv(){
     const container = document.querySelector('#grid');
     console.log(container)
     const div = document.createElement('div');
     console.log(div);
     div.classList.add('grid-item');
     container.appendChild(div);    
+}
+
+function createMultipleGridDivs(number) {
+    for (let i = 0; i < number; i++) {
+        createSingleGridDiv();
+    }
 }
