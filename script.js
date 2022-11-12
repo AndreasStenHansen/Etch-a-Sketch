679d38d9775194eeaa6056fe6b3c4744f98a1976
Andreas Sten Hansen <andreas_sten_hansen@hotmail.com>
2022-11-12 13:51:46 +0100

    Added function for updating columns in grid

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 function createSingleGridDiv(){
     const container = document.querySelector('#grid');
-    console.log(container)
+    //console.log(container)
     const div = document.createElement('div');
-    console.log(div);
+    //console.log(div);
     div.classList.add('grid-item');
     container.appendChild(div);    
 }
 
-function createMultipleGridDivs(number) {
-    for (let i = 0; i < number; i++) {
+function createMultipleGridDivs(numberofDivs) {
+    for (let i = 0; i < numberofDivs; i++) {
         createSingleGridDiv();
     }
 }
+
+function updateGridContainerTemplateColumns(columns) {
+    const gridContainer = document.querySelector('.grid-container')
+    //console.log(gridContainer)
+    let numberOfColumns = '';
+    //console.log(numberOfColumns);
+    for (let i = 0; i < columns; i++) {
+        numberOfColumns += 'auto '
+        //console.log(numberOfColumns);
+    }
+    //console.log(numberOfColumns);
+    gridContainer.style.gridTemplateColumns = numberOfColumns;
+}
+
